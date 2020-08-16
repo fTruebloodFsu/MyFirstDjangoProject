52445107a21d73c84989171f0a47b98c35155a0d
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-08-16 09:50:55 -0700

    following the polls tutorial

--- MyFirstDjangoProject/urls.py
+++ MyFirstDjangoProject/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('firstApp/', include('firstApp.urls')),
+    path('polls/', include('polls.urls'))
 ]
